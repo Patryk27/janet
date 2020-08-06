bab4d67d511f71ac923011c2b136082b743afff9
Patryk Wychowaniec <wychowaniec.patryk@gmail.com>
2020-08-06 19:53:45 +0200

    refactor: Clean-up `system/utils.rs` a bit

--- src/system/utils.rs
+++ src/system/utils.rs
@@ -1,10 +1,9 @@
-//! TODO proof of concept - requires solid refactoring
-
-use crate::database::{self as db, Database, NewUser};
+use crate::database::{self as db, Database};
 use crate::gitlab::{self as gl, GitLabClient};
 use crate::interface::{MergeRequestPtr, PtrContext};
 use anyhow::*;
 
+/// Loads user from GitLab and upserts it into the database.
 pub async fn sync_user(
     db: &Database,
     gitlab: &GitLabClient,
@@ -14,7 +13,7 @@ pub async fn sync_user(
 
     let user_id = db
         .users()
-        .add(&NewUser {
+        .add(&db::NewUser {
             ext_id: gl_user.id.inner() as _,
         })
         .await?;
@@ -22,25 +21,36 @@ pub async fn sync_user(
     Ok((gl_user, user_id))
 }
 
-pub async fn sync_merge_request(
+/// Loads project from GitLab and upserts it into the database.
+pub async fn sync_project(
     db: &Database,
     gitlab: &GitLabClient,
-    ptr: &MergeRequestPtr,
-    ptr_context: &PtrContext,
-) -> Result<(gl::Project, gl::MergeRequest, db::Id<db::MergeRequest>)> {
-    let (gl_project_id, gl_merge_request_iid) = ptr.resolve(&gitlab, ptr_context).await?;
-
+    gl_project_id: gl::ProjectId,
+) -> Result<(gl::Project, db::Id<db::Project>)> {
     let gl_project = gitlab.project(&gl_project_id.inner().to_string()).await?;
-
-    let gl_merge_request = gitlab
-        .merge_request(gl_project_id, gl_merge_request_iid)
-        .await?;
 
     let project_id = db
         .projects()
         .add(&db::NewProject {
             ext_id: gl_project_id.inner() as _,
         })
+        .await?;
+
+    Ok((gl_project, project_id))
+}
+
+/// Loads project & merge request from GitLab and upserts both into the
+/// database.
+pub async fn sync_merge_request(
+    db: &Database,
+    gitlab: &GitLabClient,
+    gl_project_id: gl::ProjectId,
+    gl_merge_request_iid: gl::MergeRequestIid,
+) -> Result<(gl::Project, gl::MergeRequest, db::Id<db::MergeRequest>)> {
+    let (gl_project, project_id) = sync_project(db, gitlab, gl_project_id).await?;
+
+    let gl_merge_request = gitlab
+        .merge_request(gl_project_id, gl_merge_request_iid)
         .await?;
 
     let merge_request_id = db
@@ -54,4 +64,16 @@ pub async fn sync_merge_request(
         .await?;
 
     Ok((gl_project, gl_merge_request, merge_request_id))
+}
+
+/// Resolves pointer to merge request and upserts it into the database.
+pub async fn sync_merge_request_ptr(
+    db: &Database,
+    gitlab: &GitLabClient,
+    merge_request: &MergeRequestPtr,
+    ptr_context: &PtrContext,
+) -> Result<(gl::Project, gl::MergeRequest, db::Id<db::MergeRequest>)> {
+    let (gl_project_id, gl_merge_request_iid) = merge_request.resolve(&gitlab, ptr_context).await?;
+
+    sync_merge_request(db, gitlab, gl_project_id, gl_merge_request_iid).await
 }
